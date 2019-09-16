263d64bdd80f02eafcf2dd1e1e835db9655cb44a
Zjinxing <1182999456@qq.com>
2019-09-16 23:08:32 +0800

    添加store

---
+++ src/Store/index.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react';
+import { remote } from 'electron';
+const { systemPreferences } = remote;
+
+export const AppContext = createContext({});
+
+export default ({ children }: any) => {
+  const setData = (name: string, data: any) => {
+    setState(prevState => {
+      return {
+        ...prevState,
+        [name]: data
+      };
+    });
+  };
+  const addState = (name: string, data: any) => {
+    setState(prevState => {
+      return {
+        ...prevState,
+        [name]: data
+      };
+    });
+  };
+  const initAppState = {
+    isDarkMode: systemPreferences.isDarkMode(),
+    setData,
+    addState
+  };
+  const [state, setState] = useState(initAppState);
+  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
+};
