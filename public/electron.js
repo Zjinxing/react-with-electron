daf7ed09a25a1d01e4673df8960d9e49baa6b312
Zjinxing <1182999456@qq.com>
2019-10-10 22:56:25 +0800

    添加header双击监听事件

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,13 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 const path = require('path')
 
 let win
 
-const URL = process.env.NODE_ENV === 'dev' ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+const URL =
+  process.env.NODE_ENV === 'dev'
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`
 
 function createWindow() {
   win = new BrowserWindow({
@@ -18,21 +18,29 @@ function createWindow() {
     titleBarStyle: 'hidden',
     webPreferences: {
       nodeIntegration: true
-    },
+    }
   })
   win.loadURL(URL)
   win.webContents.openDevTools()
-  win.on('closed', () => (win = null));
+  win.on('closed', () => (win = null))
 }
+
+ipcMain.on('maximize', () => {
+  if (win.isMaximized()) {
+    win.unmaximize()
+  } else {
+    win.maximize()
+  }
+})
 
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 app.on('activate', () => {
   if (win === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,17 @@ import React, { useContext } from 'react'
 import { AppContext, State } from 'Store/index'
 import './index.scss'
 import EqInput from './EqInput'
+import { ipcRenderer } from 'electron'
 
 const Header: React.FC = () => {
   const { isDarkMode } = useContext(AppContext) as State
+
+  const doubleClick = () => {
+    console.log('dbClick')
+    ipcRenderer.send('maximize')
+  }
   return (
-    <div className={`header-bar ${isDarkMode ? 'dark' : ''} test`}>
+    <div className={`header-bar ${isDarkMode ? 'dark' : ''} test`} onDoubleClick={doubleClick}>
       <div className="header__left">
         <div className="header__left-navbar">
           <i className="iconfont icon-back"></i>
