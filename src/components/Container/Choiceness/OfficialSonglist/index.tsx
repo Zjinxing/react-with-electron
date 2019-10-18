f200295f4f04ba229b6a9dc23f96770ae35ef984
Zjinxing <1182999456@qq.com>
2019-10-18 22:38:38 +0800

    官方歌单

---
+++ src/components/Container/Choiceness/OfficialSonglist/index.tsx
@@ -0,0 +1,32 @@
+import { remote } from 'electron'
+import React, { useEffect, useState, ReactNode } from 'react'
+import { GET_SONGLIST } from 'request/GetSongList'
+import { Playlist } from 'request/types/Playlist'
+import PlaylistCover from 'components/common/PlaylistCover/index'
+import './index.scss'
+
+const OfficialSonglist: React.FC = () => {
+  const [officialSonglist, setOfficialSonglist] = useState<Playlist>()
+  let coverList: ReactNode
+  useEffect(() => {
+    ;(async () => {
+      const songlist = await GET_SONGLIST()
+      setOfficialSonglist(songlist)
+    })()
+  }, [])
+  if (officialSonglist) {
+    const playlist = officialSonglist.playlist.data.v_playlist
+    const showList = playlist.slice(0, 6)
+    console.log(showList)
+    coverList = showList.map(item => <PlaylistCover playlistInfo={item} key={item.tid} />)
+  }
+
+  return (
+    <div className="official-songlist">
+      <h4 className="title">官方歌单</h4>
+      <ul className="cover-list">{coverList}</ul>
+    </div>
+  )
+}
+
+export default OfficialSonglist
