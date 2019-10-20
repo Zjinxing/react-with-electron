4febd0f7f5af6f6dc95d8cd05e384e80a2b3befe
Zjinxing <1182999456@qq.com>
2019-10-20 23:37:50 +0800

    添加歌单详情接口

--- src/request/types/Playlist.ts
+++ src/request/types/Playlist.ts
@@ -48,3 +48,148 @@ export interface Playlist {
     code: number
   }
 }
+
+export interface Singer {
+  id: number
+  mid: string
+  name: string
+  title: string
+}
+
+export interface SongDetail {
+  id: number
+  type: number
+  songtype: number
+  mid: string
+  name: string
+  title: string
+  subtitle: string
+  interval: number
+  isonly: number
+  language: number
+  genre: number
+  index_cd: number
+  index_album: number
+  status: number
+  fnote: number
+  url: string
+  time_public: string
+  singer: Singer[]
+  album: {
+    id: number
+    mid: string
+    name: string
+    title: string
+    subtitle: string
+  }
+  mv: {
+    id: number
+    vid: string
+  }
+  ksong: {
+    id: number
+    mid: string
+  }
+  file: {
+    media_mid: string
+    size_try: number
+    b_30s: number
+    e_30s: number
+    try_begin: number
+    try_end: number
+    size_24aac: number
+    size_48aac: number
+    size_96aac: number
+    size_192aac: number
+    size_192ogg: number
+    size_128mp3: number
+    size_320mp3: number
+    size_aac: number
+    size_ogg: number
+    size_128: number
+    size_320: number
+    size_ape: number
+    size_flac: number
+    size_dts: number
+  }
+  volume: {
+    gain: number
+    peak: number
+    lra: number
+  }
+  pay: {
+    pay_month: number
+    price_track: number
+    price_album: number
+    pay_play: number
+    pay_down: number
+    pay_status: number
+    time_free: number
+  }
+  action: {
+    switch: number
+    msgid: number
+    msgpay: number
+    alert: number
+    icons: number
+  }
+}
+
+export interface ListContent {
+  disstid: string
+  dir_show: number
+  owndir: number
+  dirid: number
+  coveradurl: string
+  dissid: number
+  login: string
+  uin: string
+  encrypt_uin: string
+  dissname: string
+  logo: string
+  pic_mid: string
+  album_pic_mid: string
+  pic_dpi: number
+  isAd: number
+  desc: string
+  ctime: number
+  mtime: number
+  headurl: string
+  ifpicurl: string
+  nick: string
+  nickname: string
+  type: number
+  singerid: number
+  isvip: number
+  isdj: number
+  tags: { id: number; name: string; pid: number }[]
+  songnum: number
+  songids: string
+  songtypes: string
+  disstype: number
+  dir_pic_url2: string
+  song_update_time: number
+  song_update_num: number
+  total_song_num: number
+  song_begin: number
+  cur_song_num: number
+  songlist: SongDetail[]
+  visitnum: number
+  cmtnum: number
+  buynum: number
+  scoreavage: string
+  scoreusercount: number
+}
+
+export interface SonglistDetail {
+  response: {
+    code: number
+    subcode: number
+    accessed_plaza_cache: number
+    accessed_favbase: number
+    login: string
+    cdnumber: number
+    cdlist: ListContent[]
+    realcdnum: number
+  }
+}
