4eeb86d59e31f285e9f7f2c9111dda494d972861
Zjinxing <1182999456@qq.com>
2019-10-23 21:23:40 +0800

    配置antd按需引入

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require('path')
+const tsImportPluginFactory = require('ts-import-plugin')
+const {
+  override,
+  setWebpackTarget,
+  addWebpackResolve,
+  addWebpackModuleRule
+} = require('customize-cra')
 
-module.exports = function override(config) {
-  config.target = 'electron-renderer';
-  const resolve = {
+module.exports = override(
+  setWebpackTarget('electron-renderer'),
+  addWebpackResolve({
     alias: {
       src: path.resolve(__dirname, 'src')
     },
     modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')]
-  }
-  config.resolve = {
-    ...config.resolve,
-    ...resolve
-  }
-  return config;
-}
+  }),
+  addWebpackModuleRule({
+    test: /\.(jsx|tsx|js|ts)$/,
+    loader: 'ts-loader',
+    options: {
+      transpileOnly: true,
+      getCustomTransformers: () => ({
+        before: [
+          tsImportPluginFactory({
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: 'css'
+          })
+        ]
+      }),
+      compilerOptions: {
+        module: 'es2015'
+      }
+    },
+    exclude: /node_modules/
+  })
+)
