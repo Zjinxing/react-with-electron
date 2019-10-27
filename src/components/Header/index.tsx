ee455ff2b70d6ebd2922f5a42592ff3c7f93f95a
Zjinxing <1182999456@qq.com>
2019-10-27 21:24:47 +0800

    添加前进后退刷新

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,39 @@
 import React, { useContext } from 'react'
+import { ipcRenderer } from 'electron'
+import { createBrowserHistory } from 'history'
 import { AppContext, State } from 'Store/index'
 import './index.scss'
 import EqInput from './EqInput'
-import { ipcRenderer } from 'electron'
 
 const Header: React.FC = () => {
   const { isDarkMode } = useContext(AppContext) as State
+  const history = createBrowserHistory()
 
   const doubleClick = () => {
     console.log('dbClick')
     ipcRenderer.send('maximize')
   }
+
+  const goBack = () => {
+    history.goBack()
+  }
+
+  const goForward = () => {
+    history.goForward()
+  }
+
+  const refresh = () => {
+    console.log(history)
+    history.go(0)
+  }
+
   return (
     <div className={`header-bar ${isDarkMode ? 'dark' : ''} test`} onDoubleClick={doubleClick}>
       <div className="header__left">
         <div className="header__left-navbar">
-          <i className="iconfont icon-back"></i>
-          <i className="iconfont icon-forward"></i>
-          <i className="iconfont icon-refresh"></i>
+          <i onClick={goBack} className="iconfont icon-back"></i>
+          <i onClick={goForward} className="iconfont icon-forward"></i>
+          <i onClick={refresh} className="iconfont icon-refresh"></i>
         </div>
         <EqInput />
       </div>
