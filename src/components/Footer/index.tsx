f564b72eed4cc137f8123f1cf203c947b742f0b4
Zjinxing <1182999456@qq.com>
2019-10-31 09:03:02 +0800

    播放进度条

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,54 +1,97 @@
-import React, { useContext, useEffect } from 'react'
-import { AppContext, State } from '../../Store/index'
-import './index.scss'
+import React, { useContext, useEffect, useState, SyntheticEvent } from 'react'
+import { AppContext, State } from 'Store/index'
 import { GET_MUSIC_VKEY } from 'request/GetSongList'
+import Progress from 'components/common/Progress'
+import { formatSeconds } from 'utils'
+import './index.scss'
+
 const Footer: React.FC = () => {
-  const {
-    isDarkMode,
-    currentSongUrl,
-    currentSongMId,
-    playlistMids,
-    playMode,
-    setData
-  } = useContext(AppContext) as State
+  const { isDarkMode, currentSongUrl, currentSongId, playlist, playMode, setData } = useContext(
+    AppContext
+  ) as State
+
+  const [albumCover, setAlbumCover] = useState('') // 当前歌曲封面图url
+  const [currentTime, setCurrentTime] = useState(0) // 当前歌曲播放时间
+  const [duration, setDuration] = useState(0) // 当前歌曲总时长
+  const [percent, setPercent] = useState(0) // 播放进度
+  const [songName, setSongName] = useState('')
 
   const audioRef = React.createRef<HTMLAudioElement>()
 
+  // 检测到当前歌曲的id发生变化开始播放
   useEffect(() => {
-    console.log(currentSongMId)
     audioRef.current && audioRef.current.play()
-    console.log('idbmhw')
-  }, [currentSongMId])
+    const currentSong = playlist.find(item => item.id === currentSongId)
+    const { album, name, singer } = currentSong || {}
+    album &&
+      setAlbumCover(
+        `https://y.gtimg.cn/music/photo_new/T002R800x800M000${album.mid}.jpg?max_age=2592000`
+      )
+    const singerName = singer && singer.map(item => item.name).join('/')
+    name && setSongName(`${name} - ${singerName}`)
+  }, [currentSongId])
 
   const playNext = async () => {
-    console.log('播放下一首')
-    console.log({ playlistMids })
-    const currentIndex = playlistMids.findIndex(item => item === currentSongMId)
-    console.log({ currentSongMId })
-    if (playMode === 'order') {
-      if (currentIndex === playlistMids.length) {
-        const currentSong = await GET_MUSIC_VKEY({ songmid: playlistMids[0] })
+    const currentIndex = playlist.findIndex(item => item.id === currentSongId)
+    console.log({ currentSongId })
+    if (playMode === 'order' || playMode === 'loop') {
+      if (currentIndex === playlist.length) {
+        const currentSong = await GET_MUSIC_VKEY({ songmid: playlist[0].mid })
         setData({
           currentSongUrl: currentSong.response.playLists[0],
-          currentSongMId: playlistMids[0]
+          currentSongMId: playlist[0]
         })
       } else {
-        const currentSong = await GET_MUSIC_VKEY({ songmid: playlistMids[currentIndex + 1] })
+        const currentSong = await GET_MUSIC_VKEY({ songmid: playlist[currentIndex + 1].mid })
+        console.log(' //////////////// ', currentSong)
         setData({
           currentSongUrl: currentSong.response.playLists[0],
-          currentSongMId: playlistMids[currentIndex + 1]
+          currentSongMId: playlist[currentIndex + 1]
         })
       }
     }
   }
 
   const playPreview = async () => {
-    console.log('播放上一首')
+    const currentIndex = playlist.findIndex(item => item.id === currentSongId)
+    if (playMode === 'order' || playMode === 'loop') {
+      if (currentIndex === 0) {
+        const currentSong = await GET_MUSIC_VKEY({ songmid: playlist[playlist.length - 1].mid })
+        setData({
+          currentSongUrl: currentSong.response.playLists[0],
+          currentSongMId: playlist[playlist.length - 1]
+        })
+      } else {
+        const currentSong = await GET_MUSIC_VKEY({ songmid: playlist[currentIndex - 1].mid })
+        setData({
+          currentSongUrl: currentSong.response.playLists[0],
+          currentSongMId: playlist[currentIndex - 1]
+        })
+      }
+    }
   }
 
   const togglePlay = () => {
     console.log('播放/暂停')
     audioRef.current!.paused ? audioRef.current!.play() : audioRef.current!.pause()
+  }
+
+  const handleCanPlay = (e: SyntheticEvent<HTMLAudioElement, Event>) => {
+    const target = e.target as HTMLAudioElement
+    console.log('准备好播放', target.duration)
+    const duration = target.duration
+    setDuration(duration)
+  }
+
+  const onProgress = (e: SyntheticEvent<HTMLAudioElement>) => {
+    const target = e.target as HTMLAudioElement
+    setInterval(() => {
+      const current = target.currentTime
+      const percent = duration && (current / duration) * 100
+      percent && setPercent(percent)
+      console.log(percent)
+      setCurrentTime(current)
+    }, 250)
   }
 
   return (
@@ -80,10 +123,22 @@ const Footer: React.FC = () => {
         </span>
       </div>
       <div className="footer-progress">
-        <audio ref={audioRef} src={currentSongUrl} controls></audio>
-        ====== {currentSongMId} ======
+        <img src={albumCover} alt="" width="40" height="40" className="footer-progress-cover" />
+        <div className="footer-progress-content">
+          <span className="song-name">{songName}</span>
+          <span className="song-time">
+            {formatSeconds(currentTime)} / {formatSeconds(duration)}
+          </span>
+          <Progress width={percent}></Progress>
+        </div>
       </div>
       <div className="footer-operator"></div>
+      <audio
+        ref={audioRef}
+        src={currentSongUrl}
+        onCanPlay={handleCanPlay}
+        onTimeUpdate={onProgress}
+      ></audio>
     </div>
   )
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export const formatSeconds = (val: number): string => {
   const min = ~~(val / 60)
-  const sec = val % 60
+  const sec = ~~(val % 60)
   return `${min}:${sec}`.replace(/\b\d\b/g, '0$&')
 }
