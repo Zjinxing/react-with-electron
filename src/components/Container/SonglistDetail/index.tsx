081af77c40de537422a097979d46bce719d695aa
Zjinxing <1182999456@qq.com>
2019-11-16 21:02:25 +0800

    歌单头部描述信息分离为公共组件

--- src/components/Container/SonglistDetail/index.tsx
+++ src/components/Container/SonglistDetail/index.tsx
@@ -7,6 +7,7 @@ import { SonglistDetail, SongDetail } from 'request/types/Playlist'
 import { SongListComments } from 'request/types/Comments'
 import SonglistTable from 'components/common/Songlist'
 import MyButton from 'components/common/MyButton'
+import SongDesc from 'components/common/SongDesc'
 import { AppContext, State } from 'Store'
 import './index.scss'
 
@@ -112,55 +113,16 @@ const SonglistDetailFC: React.FC<RouteComponentProps> = props => {
     const cd = songlistDetail.response.cdlist[0]
     SongTable = <SonglistTable songTableData={cd.songlist} togglePlay={togglePlay}></SonglistTable>
 
-    SonglistDesc = (
-      <>
-        <img src={cd.logo} alt={cd.dissname} width="150" className="cover" />
-        <div className="songlist-info">
-          <h2 className="songlist-info-title">{cd.dissname}</h2>
-          <div className="songlist-info-summary">
-            <span className="creator-nickname">{cd.nickname}</span>
-            <span className="songlist-info-summary--createtime">
-              {dayjs(cd.ctime * 1000).format('YYYY-MM-DD')}创建
-            </span>
-            <ul className="songlist-info-summary--tags">
-              {cd.tags.map(tag => (
-                <li key={tag.id} onClick={() => tagClick(tag)}>
-                  {tag.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <p className="songlist-info-desc">
-            <span>{cd.desc.replace(/(&nbsp;)|(&#160;)/g, ' ')}</span>
-            <span className="unfold">[展开]</span>
-          </p>
-          <div className="songlist-info-control">
-            <MyButton type="primary" onClick={playAll}>
-              <img src={require('resources/cellPlay_hover@2x.png')} width="16" alt="" />
-              播放全部
-            </MyButton>
-            <MyButton>
-              &nbsp;
-              <img src={require('resources/cellLoveUnselected_hl@2x.png')} width="16" alt="" />
-              收藏 &nbsp;
-            </MyButton>
-            <MyButton>
-              <img src={require('resources/batch_hl.png')} width="16" alt="" />
-              批量操作
-            </MyButton>
-            <MyButton>
-              <img src={require('resources/cellDownload_hl@2x.png')} width="18" alt="" />
-              下载全部
-            </MyButton>
-            <MyButton>
-              &nbsp;
-              <img src={require('resources/pop_share_hl@2x.png')} width="14" alt="" />
-              &nbsp; 分享 &nbsp;
-            </MyButton>
-          </div>
-        </div>
-      </>
-    )
+    const description = {
+      cover: cd.logo,
+      name: cd.dissname,
+      creator: cd.nickname,
+      createdAt: dayjs(cd.ctime * 1000).format('YYYY-MM-DD'),
+      desc: cd.desc,
+      tags: cd.tags
+    }
+    SonglistDesc = <SongDesc description={description} tagClick={tagClick} playAll={playAll} />
+
     HeaderSummary = (
       <div className={`header-summary ${headerClass}`}>
         <div className="header-summary-name">
@@ -193,9 +155,7 @@ const SonglistDetailFC: React.FC<RouteComponentProps> = props => {
 
   return (
     <div className={`songlist-detail ${isDarkMode ? 'dark-mode' : ''}`}>
-      <div ref={headerRef} className="songlist-detail-header">
-        {SonglistDesc}
-      </div>
+      <div ref={headerRef}>{SonglistDesc}</div>
       {HeaderSummary}
       <Tabs defaultActiveKey="song">
         <TabPane tab={`歌曲 (${songNum})`} key="song">
