5a06de0fe0d7996752b87598699dbed0de7ef177
Zjinxing <1182999456@qq.com>
2019-11-19 08:44:28 +0800

    接口公共配置

---
+++ src/request/config.ts
@@ -0,0 +1,12 @@
+export const commonConfig = {
+  '-': '0',
+  g_tk: 5381,
+  loginUin: 0,
+  hostUin: 0,
+  format: 'json',
+  inCharset: 'utf8',
+  outCharset: 'utf-8',
+  notice: 0,
+  platform: 'yqq.json',
+  needNewCode: 0
+}
