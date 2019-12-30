32c31a28ca7e973cfc69ab53809416757b422205
Zjinxing <1182999456@qq.com>
2019-12-30 22:28:44 +0800

    format

--- src/components/Container/Ranking/index.tsx
+++ src/components/Container/Ranking/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Ranking: React.FC = () => {
-  return <div className="ranking">排行榜</div>;
-};
+  return <div className="ranking">排行榜</div>
+}
 
-export default Ranking;
+export default Ranking
